b07f294f74ff4978badeba5aad30900819133d00
durgababu <durgababu.nallagachu@gmail.com>
2025-09-28 02:17:35 +0530

    adding roboshop infra

---
+++ 40-eks/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "roboshop-project-durgababu"
+    key    = "runner-roboshop-dev-eks"
+    region = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
